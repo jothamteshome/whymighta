a8e0b3afe3a3e98a157c9420ec3673ebc5885c97
Jotham Teshome <jothamteshome@gmail.com>
2026-04-30 09:59:37 -0400

    chore: update deployment workflow and scripts for improved reliability and AWS integration

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 if [ "$ENV" = "production" ]; then
     echo "Creating database if not exists..."
     psql "postgresql://$DB_USERNAME:$DB_PASSWORD@$DB_HOST:$DB_PORT/postgres" \
-        -c "CREATE DATABASE \"$DB_DATABASE\";" 2>/dev/null || true
+        -c "CREATE DATABASE \"$DB_DATABASE\";" || true
 fi
 
 exec python main.py
