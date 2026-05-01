f25234584307d2f2123fa5db0a83c44828367df0
Jotham Teshome <jothamteshome@gmail.com>
2026-04-30 21:55:22 -0400

    chore: remove postgresql-client installation and database creation logic from entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 set -e
-
-if [ "$ENV" = "production" ]; then
-    echo "Creating database if not exists..."
-    PGPASSWORD="$DB_PASSWORD" psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USERNAME" -d postgres \
-    -c "CREATE DATABASE \"$DB_DATABASE\";" || true
-fi
-
 exec python main.py
