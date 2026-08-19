cf71ce8cea346915812c2f91ed83b216d68d5336
Jotham Teshome <jothamteshome@gmail.com>
2026-08-18 23:24:40 -0400

    feat: add logging to minecraft utils

--- utils/minecraft.py
+++ utils/minecraft.py
@@ -1,5 +1,7 @@
-import aiohttp
+import aiohttp, logging
 from core.config import config
+
+logger = logging.getLogger(__name__)
 
 
 def get_headers() -> dict:
@@ -16,6 +18,8 @@ async def get_status() -> dict:
 async def start_server(name: str) -> dict:
     async with aiohttp.ClientSession() as session:
         async with session.post(f"{config.MINECRAFT_API_URL}/start", headers=get_headers(), json={"server": name}) as resp:
+            text = await resp.text()
+            logger.info(f"Status: {resp.status}, Body: {text}")
             resp.raise_for_status()
             return await resp.json()
 
@@ -23,5 +27,7 @@ async def start_server(name: str) -> dict:
 async def stop_server(name: str) -> dict:
     async with aiohttp.ClientSession() as session:
         async with session.post(f"{config.MINECRAFT_API_URL}/stop", headers=get_headers(), json={"server": name}) as resp:
+            text = await resp.text()
+            logger.info(f"Status: {resp.status}, Body: {text}")
             resp.raise_for_status()
             return await resp.json()
